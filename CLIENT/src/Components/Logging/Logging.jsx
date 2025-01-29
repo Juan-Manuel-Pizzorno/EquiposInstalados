0670fd2d8ffea6ca30d5c688794807f8de1933c9
Juan Manuel Pizzorno <juan_19_2013@hotmail.com>
2025-01-29 16:27:13 -0300

    creando mas cosas del login

--- CLIENT/src/Components/Logging/Logging.jsx
+++ CLIENT/src/Components/Logging/Logging.jsx
@@ -1,14 +1,45 @@
 import styles from "./logging.module.css";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";//Se usa para manejar el estado del usuario, contraseña y errores.
+import miImagen from "../../assets/laQuimica.jpg"
+
+
 
 export default function Logging() {
+
+  const [usuario, setUsuario] = useState("");
+const [password, setPassword] = useState("");
+const [error, setError] = useState("");
   
   return (
     <div>
-      <h1>
-        <p>La Quimica Quirúrgica</p>
-      </h1>
-      <p>Haz click para ingresar</p>
-    </div>
+    <h2>Login</h2>
+    {error && <p style={{ color: "red" }}>{error}</p>}
+    <form className={styles.login}>
+      <img 
+      src={miImagen} 
+     className={styles.laQuimica}
+      alt="" />
+      <input
+        type="text"
+        className={styles.controls}
+        placeholder="Usuario"
+        value={usuario}
+        onChange={(e) => setUsuario(e.target.value)}
+      />
+      <input
+        type="password"
+        className={styles.controls}
+        placeholder="Contraseña"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button 
+        type="submit"
+        className={styles.button}
+        >Ingresar
+      </button>
+    </form>
+  </div>
   );
 }
